e5cbf850bdfa7cd72e9ba9ad14807abe9f3e0cca
Lisa Hewus Fresh <lhewusfresh@gmail.com>
2015-02-06 19:58:30 -0800

    Bug 1089713 - Updates and additions

--- airmozilla/starred/models.py
+++ airmozilla/starred/models.py
@@ -1,16 +1,12 @@
 from django.core.cache import cache
 from django.db import models
 from django.contrib.auth.models import User
+from airmozilla.main.models import Event
 
-from airmozilla.main.models import Event, SuggestedEvent
 
-
-class Starred(models.Model):
+class StarredEvents(models.Model):
     event = models.ForeignKey(Event)
-    user = models.ForeignKey(User, null=True)
-    added = models.DateTimeField(auto_now_add = True)
+    user = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now_add=True)
 
 
-    @property
-    def anonymous(self):
-        return not self.user_id
