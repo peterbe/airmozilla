e0a5e21a03290faacc7305ba8faf49f117e38ee5
Lisa Hewus Fresh <lhewusfresh@gmail.com>
2015-02-20 23:06:45 -0800

    Bug 1089713 - Proof of concept. Unfinished push for comments.

--- airmozilla/starred/urls.py
+++ airmozilla/starred/urls.py
@@ -1,9 +1,12 @@
+from django.views.generic.base import RedirectView
 from django.conf.urls import patterns, url
 
 from . import views
 
 urlpatterns = patterns(
     '',
+    url(r'^page/1/$', RedirectView.as_view(url='/starred/'), name='first_starred_page'),
+    url(r'^page/(?P<page>\d+)/$', views.home, name='home'),
     url(r'^$', views.home, name='home'),
     url(r'^sync/$', views.sync_starred_events, name='sync_starred_events'),
 )
