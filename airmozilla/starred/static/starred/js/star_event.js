8f93d13cf1f6980dead8f9d204d4eb4694eda0b6
Lisa Hewus Fresh <lhewusfresh@gmail.com>
2015-02-26 13:37:39 -0800

    Bug 1089713 - Unfinished push for review and comments

--- airmozilla/starred/static/starred/js/star_event.js
+++ airmozilla/starred/static/starred/js/star_event.js
@@ -1,6 +1,37 @@
 $(function() {
+    var stars = [];
+
+    function is_starred(id) {
+        $(id).is('a.star.star-on');
+    }
+    function star(id) {
+        $(id).toggleClass('star-on');
+    }
+    function unstar(id) {
+        $(id).toggleClass('star');
+    }
+    function sync_stars() {
+        // add to local storage
+        $('a.star').each(function () {
+            var e = $(this);
+            var id = e.data('id');
+            e.toggleClass('star-on', is_starred(id));
+        });
+    }
+
     $('.entry-summary').on('click','a.star', function () {
-        console.log($(this));
-        $(this).toggleClass('star-off');
+        var e = $(this);
+        var id = e.data('id');
+        console.log(e);
+        console.log(id);
+        stars.push(id);
+        console.log(stars);
+
+        if (is_starred(id))
+            unstar(id);
+        else
+            star(id);
+
+        e.toggleClass('star-on');
     });
 });
