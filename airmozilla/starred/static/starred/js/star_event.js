83fe14dd0a6e4e0d7e101d7cadb9679098f3a0f5
Lisa Hewus Fresh <lhewusfresh@gmail.com>
2015-03-01 20:21:39 -0800

    Bug 1089713 - Add stars to live events and sidebar

--- airmozilla/starred/static/starred/js/star_event.js
+++ airmozilla/starred/static/starred/js/star_event.js
@@ -1,37 +1,75 @@
 $(function() {
-    var stars = [];
+    var stars = [],
+    csrf_token,
+    isLoggedIn,
+    from_browser = localStorage.getItem('stars');
 
-    function is_starred(id) {
-        $(id).is('a.star.star-on');
-    }
-    function star(id) {
-        $(id).toggleClass('star-on');
-    }
-    function unstar(id) {
-        $(id).toggleClass('star');
-    }
-    function sync_stars() {
-        // add to local storage
-        $('a.star').each(function () {
-            var e = $(this);
-            var id = e.data('id');
-            e.toggleClass('star-on', is_starred(id));
+
+   function triggerClickedStars() {
+        $('a.star').each(function(i, element) {
+            var index = stars.indexOf($(element).data('id'))
+            if (index > -1) {
+                $(element).addClass('star-on')
+            }
+            else {
+                $(element).removeClass('star-on')
+            }
         });
-    }
+   }
 
-    $('.entry-summary').on('click','a.star', function () {
-        var e = $(this);
-        var id = e.data('id');
-        console.log(e);
-        console.log(id);
-        stars.push(id);
-        console.log(stars);
+   function initialSync() {
+       $.getJSON('/starred/sync/').then(function(response) {
+           if (response) {
+               csrf_token = response.csrf_token;
+               isLoggedIn = response.isLoggedIn;
+               stars.concat(response.ids);
+               if (csrf_token)
+                  sync();
+           }
+       });
+   }
 
-        if (is_starred(id))
-            unstar(id);
-        else
-            star(id);
+   function sync() {
+      if (!csrf_token)
+          return initialSync();
+      if (!isLoggedIn)
+          return;
 
-        e.toggleClass('star-on');
-    });
+// Can also set this to $.param({'ids': stars, 'csrfmiddlewaretoken': csrf_token}, true).then(
+// and remove the square brackets from starred/views.py line 30
+// Or manually encode and decode JSON. See http://api.jquery.com/jQuery.param/
+       $.post('/starred/sync/', {'ids': stars, 'csrfmiddlewaretoken': csrf_token}).then(
+               function(response) {
+                   if (response) {
+                       csrf_token = response.csrf_token;
+                       isLoggedIn = response.isLoggedIn;
+                       stars = response.ids;
+                       localStorage.setItem('stars', JSON.stringify(stars));
+                       triggerClickedStars();
+                   }
+               });
+   }
+
+   function toggleArrayPresence(id) {
+      var index = stars.indexOf(id);
+      if (index > -1) {
+         stars.splice(index, 1);
+      } else {
+         stars.push(id);
+      }
+      localStorage.setItem('stars', JSON.stringify(stars));
+      sync();
+   }
+
+
+   $('a.star').on('click', function () {
+      toggleArrayPresence($(this).data('id'));
+      $(this).toggleClass('star-on');
+   });
+
+   if (from_browser) {
+       stars = JSON.parse(from_browser);
+       triggerClickedStars();
+   }
+   sync();
 });
